4a8c10e08d51520adc2b340c23d5a9761384791c
Josh Crowther <jshcrowthe@gmail.com>
2015-12-02 11:11:39 -0500

    Add initial load

--- server.js
+++ server.js
@@ -9,5 +9,9 @@ app.get('/*', function(req, res){
 var io = socketio.listen(app.listen(3000));
 
 io.sockets.on('connection', function(socket) {
-  // Do Stuff
+  // When a client first connects, send all existing messsages to the client
+  r.connect({db: 'rethinkdb_chat'})
+  .then((conn)   => r.table('messages').orderBy('timestamp').run(conn).finally(() => conn.close()))
+  .then((cursor) => cursor.toArray())
+  .then((result) => socket.emit('load messages', result));
 });
