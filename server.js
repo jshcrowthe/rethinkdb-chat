d9015dbabfa2b163cfa25891929f8a175b028ee3
Josh Crowther <jshcrowthe@gmail.com>
2015-12-02 11:11:06 -0500

    Stub out socket.io stuff

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ var r = require('rethinkdb');
 app.get('/*', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+var io = socketio.listen(app.listen(3000));
+
+io.sockets.on('connection', function(socket) {
+  // Do Stuff
+});
